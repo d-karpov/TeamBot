44cf9c1d893eec96155b1eeb326fef253e56228f
d-karpov <77918194+d-karpov@users.noreply.github.com>
2022-09-04 14:02:11 +0300

    Replying function and small fixes: - replying to teammates from leads was added - context type at handlers was defined - handler for reply was added

--- handlers.py
+++ handlers.py
@@ -1,19 +1,32 @@
 from telegram import Update
-from telegram.ext import MessageHandler, CommandHandler, Filters
+from telegram.ext import MessageHandler, CommandHandler, Filters, CallbackContext
 from settings import WELCOME_MESSAGE, TEAM_LEADS_CHAT_ID
 
 
-def start(update: Update, context):
+def start(update: Update, context: CallbackContext):
     update.message.reply_text(f'{WELCOME_MESSAGE.format(update.effective_user.full_name)}')
 
 
-def forward_to_leads(update: Update, context):
+def forward_to_leads(update: Update, context: CallbackContext):
     update.message.forward(chat_id=TEAM_LEADS_CHAT_ID)
+
+
+def reply_to_teammate(update: Update, context: CallbackContext):
+    if update.message.forward_from:
+        user_id = update.message.reply_to_message.forward_from.id
+    context.bot.copy_message(
+        mesage_id=update.message.message_id,
+        chat_id=user_id,
+        from_chat_id=update.message.chat_id
+    )
 
 
 def setup_dispatcher(dispatcher):
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(
         MessageHandler(Filters.chat_type.private, forward_to_leads)
+    )
+    dispatcher.add_handler(
+        MessageHandler(Filters.chat(TEAM_LEADS_CHAT_ID) & Filters.reply, reply_to_teammate)
     )
     return dispatcher
