0754284bc21f3f2141b2e0a6d789df1aa524af66
d-karpov <77918194+d-karpov@users.noreply.github.com>
2022-09-04 18:50:17 +0300

    Changed env parameters name as per heroku convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater
-from settings import TOKEN, PORT, HEROKU_APP_NAME
+from settings import TOKEN, PORT, APP_NAME_HEROKU
 from handlers import setup_dispatcher
 
 
@@ -10,6 +10,6 @@ updater.start_webhook(
         listen="0.0.0.0",
         port=PORT,
         url_path=TOKEN,
-        webhook_url=f"https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}"
+        webhook_url=f"https://{APP_NAME_HEROKU}.herokuapp.com/{TOKEN}"
     )
 updater.idle()
